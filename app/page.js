cdcc12dfea8eee2582cd5d6aa08fed4606da0951
Rohan Reddy <rohanreddy888@gmail.com>
2022-11-10 04:27:20 +0530

    Initial Commit

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-2xl underline">Hello World</h1>
+      <h1 className="text-2xl underline">Hello World</h1>
     </>
   );
 }
