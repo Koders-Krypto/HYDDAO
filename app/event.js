10f2aea038fc0effe2d2ee43d56ef7c1d03fa0f1
Rohan Reddy <rohanreddy888@gmail.com>
2022-11-28 23:52:31 +0530

    UI Tweaks & Responsive Changes

--- app/event.js
+++ app/event.js
@@ -25,15 +25,15 @@ export default async function Event() {
 
   return (
     <div className="flex flex-col justify-center items-center py-8">
-      <h1 className="text-3xl">
-        Upcoming Events in <strong>Hyderabad</strong>
+      <h1 className="lg:text-3xl text-5xl pb-6 pt-10 lg:pt-0 lg:pb-0">
+        Upcoming Events in <strong className="underline">Hyderabad</strong>
       </h1>
 
-      <div className="flex flex-col lg:grid lg:grid-cols-4 grid-row-auto gap-4 my-4 mx-8">
+      <div className="flex flex-col w-full lg:grid lg:grid-cols-4 grid-row-auto gap-4 my-4 mx-8 px-8">
         {data.data?.map((event, index) => {
           return (
-            <div className="shadow-md rounded-sm" key={index}>
-              <div className="w-full h-56 relative">
+            <div className="shadow-md rounded-sm bg-white" key={index}>
+              <div className="w-full h-96 lg:h-56 relative">
                 <Image
                   className="bg-cover rounded-t-sm"
                   src={
@@ -45,8 +45,10 @@ export default async function Event() {
                 />
               </div>
               <div className="m-3">
-                <h1 className="font-semibold text-lg truncate">{event.name}</h1>
-                <h1 className="font-semibold text-lg">
+                <h1 className="font-semibold text-2xl lg:text-lg truncate">
+                  {event.name}
+                </h1>
+                <h1 className="font-normal text-xl lg:text-base text-gray-600">
                   {moment(event.eventDate).format("MMMM Do YYYY")}
                 </h1>
               </div>
@@ -55,9 +57,11 @@ export default async function Event() {
         })}
       </div>
       {data.status === "Failure" && (
-        <h1 className="text-2xl py-8">Sorry 🥺 no upcoming events</h1>
+        <h1 className="text-2xl pb-20 pt-12">
+          Sorry 🥺 there is no upcoming events
+        </h1>
       )}
-      <p>
+      <p className="text-xl">
         Powered by{" "}
         <a
           className="font-bold underline"
--- app/page.js
+++ app/page.js
@@ -6,21 +6,27 @@ export default function Home() {
       <div className="flex items-start justify-center min-h-screen bg-no-repeat bg-cover background">
         <div className="flex flex-col h-[100vh] justify-between items-center layer space-y-12 py-20">
           <div className="shadow-xl rounded-full bg-white p-[1px]">
-            <Image src={"/logo.png"} alt="logo" width={150} height={150} />
+            <div className="relative w-96 h-96 lg:w-56 lg:h-56">
+              <Image src={"/logo.png"} alt="logo" fill />
+            </div>
           </div>
-          <h1 className="text-6xl text-center text-white leading-normal uppercase w-3/4">
-            Just a bunch of 🤟 #Web3 enthusiasts from Hyderabad
+          <h1 className="lg:text-5xl text-7xl text-center text-white uppercase w-full lg:w-3/4 px-8">
+            Just a bunch of 🤟 #Web3 enthusiasts from Hyderabad.
           </h1>
-          <h2 className="text-2xl text-center text-white">#WAGBI 🚀</h2>
+
+          <h2 className="flex flex-col text-6xl lg:text-4xl text-center text-white">
+            Let{"'"}s make Hyderabad Web3 capital of India.
+            <span className="pt-6"> #WAGBI 🚀</span>
+          </h2>
           <a href="#Events">
-            <div className="border-[1px] px-4 py-6 animate-pulse cursor-pointer">
+            <div className="border-[4px] lg:border-[1px]  px-4 py-6 animate-pulse cursor-pointer">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="w-6 h-6 text-white">
+                className="lg:w-6 lg:h-6 w-20 h-20 text-white">
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
